3cf5a055f8a2770a61f2ce04339abdd59477b00c
Anggistwnn <anggistwnn@gmail.com>
2022-04-21 20:22:10 +0700

    update

--- js/script.js
+++ js/script.js
@@ -23,13 +23,13 @@ function searchMovie() {
                             <div class="card-body">
                                 <h5 class="card-title">` + data.Title + `</h5>
                                 <h6 class="card-subtitle mb-2 text-muted">` + data.Year + `</h6>
-                                <a href="#" class="card-link" data-toggle="modal" data-target="#exampleModal">See Detail</a>
+                                <a href="#" class="card-link see-detail" data-toggle="modal" data-target="#exampleModal" data-id="` + data.imdbID + `">See Detail</a>
                             </div>
                         </div>
                     </div>
                     `)
 
-                })
+                });
 
                 $('#search-input').val('');
 
@@ -39,7 +39,7 @@ function searchMovie() {
                      <h1 class="text-center">` + result.Error + `</h1>
                 </div>
                 `)
-            }
+            };
         }
     });
 }
@@ -53,4 +53,43 @@ $('#search-input').on('keyup', function (e) {
 
     };
 
+});
+
+
+$('#movie-list').on('click', '.see-detail', function () {
+    $.ajax({
+        url: 'http://omdbapi.com',
+        dataType: 'json',
+        type: 'get',
+        data: {
+            'apikey': '21cf67eb',
+            'i': $(this).data('id'),
+        },
+        success: function (movie) {
+            if (movie.Response === "True") {
+
+                $('.modal-body').html(`
+                    <div class"container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="` + movie.Poster + `" class="img-fluid">
+                            </div>  
+ 
+                            <div class="col-md-8">
+                                <ul class="list-group">
+                                    <li class="list-group-item"><h1>  ` + movie.Title + `  </h1></li>
+                                    <li class="list-group-item"> Released : ` + movie.Released + ` </li>
+                                    <li class="list-group-item"> Genre : ` + movie.Genre + ` </li>
+                                    <li class="list-group-item"> Director : ` + movie.Director + ` </li>
+                                    <li class="list-group-item"> Actors : ` + movie.Actors + ` </li>
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                  `);
+
+            }
+        }
+    })
 });
