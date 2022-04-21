4ab46eaefd19d20879822a9bf1470a6327821ef3
Anggistwnn <anggistwnn@gmail.com>
2022-04-21 16:44:54 +0700

    update - can search the movies

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // membuat api connecton menggunakan ajax agar lebih mudah dari pada jsnya sendiri karna parameter aslinya json adalah ajax
 
 $('#search-button').on('click', function () {
+    $('#movie-list').html('');
+
     $.ajax({
         url: 'http://omdbapi.com',
         type: 'get',
@@ -10,7 +12,35 @@ $('#search-button').on('click', function () {
             's': $('#search-input').val()
         },
         success: function (result) {
-            console.log(result);
+            if (result.Response == "True") {
+                const movies = result.Search;
+
+                $.each(movies, function (i, data) {
+
+                    $('#movie-list').append(`
+                    <div class="col-md-4">
+                        <div class="card mb-3" style="width: 18rem;">
+                            <img src="` + data.Poster + `" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">` + data.Title + `</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">` + data.Year + `</h6>
+                                <a href="#" class="card-link">See Detail</a>
+                            </div>
+                        </div>
+                    </div>
+                    `)
+
+                })
+
+                $('#search-input').val('');
+
+            } else {
+                $('#movie-list').html(`
+                <div class"col">
+                     <h1 class="text-center">` + result.Error + `</h1>
+                </div>
+                `)
+            }
         }
     });
 });
